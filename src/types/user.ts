e1f86d7722da2c47f8d071b90283fb2ac312cd9d
Brama  Hendra Mahendra <jdoe@example.com>
2025-10-06 11:24:04 +0700

    penambahan role

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,9 +1,18 @@
+export interface Role {
+  id: number;
+  nama_role: string;
+  deskripsi: string;
+}
+
 export interface User {
   id: number;
   username: string;
   nama_lengkap: string;
-  email: string;
-  role: string;
+  nomor_telepon: string;
+  nomor_whatsapp: string;
+  tanggal_awal_kerja: string;
+  status_karyawan: string;
+  role: Role;
 }
 
 export interface LoginResponse {
